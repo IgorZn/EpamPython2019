cc00ae2f35c24d8b29e388c33eef9dec9391f2da
Igor Znamensky <igor.znamensky@gmail.com>
2019-05-31 17:23:54 +0300

    Гипотеза Коллатца.

--- 03-fp-decorator/hw/hw3.py
+++ 03-fp-decorator/hw/hw3.py
@@ -1,13 +1,14 @@
-def collatz_steps(n, count=0):
+count = 0
+def collatz_steps(n, count=count):
     if n == 1:
-        print(count)
+        global a
+        a = count
         return count
     elif n % 2 == 0:
         collatz_steps((n//2), count+1)
     else:
         collatz_steps((n * 3) + 1, count+1)
-
-
+    return a
 
 assert collatz_steps(16) == 4
 assert collatz_steps(12) == 9
