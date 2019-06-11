f76d0ef0277058b1dff368d985a4be52fb8be4d7
Igor Znamensky <igor.znamensky@gmail.com>
2019-06-11 16:09:50 +0300

    05, counter

--- 05-OOP_Exceptions/hw/counter.py
+++ 05-OOP_Exceptions/hw/counter.py
@@ -12,6 +12,37 @@ reset_instances_counter - сбросить счетчик экземпляров
 
 def instances_counter(cls):
     """Some code"""
+    setattr(cls, 'counter', 0)
+
+    def __init__(self):
+        cls.counter += 1
+
+    def get_created_instances(self=None) -> int:
+        """
+        возвращает количество созданых экземпляров класса
+        :param self:
+        :return: int
+        """
+        print(cls.counter)
+        return cls.counter
+
+    def reset_instances_counter(self=None) -> int:
+        """
+        сбросить счетчик экземпляров, возвращает значение до сброса
+        :param self:
+        :return: int
+        """
+        tmp_cnt = cls.counter
+        setattr(cls, 'counter', 0)
+        print(tmp_cnt)
+        return tmp_cnt
+
+    methods = [__init__, get_created_instances, reset_instances_counter]
+
+    # add methods to class
+    for method in methods:
+        setattr(cls, method.__name__, method)
+
     return cls
 
 
