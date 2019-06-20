a59f212cfc9c917cb0746af476043d606af62bb0
Igor Znamensky <igor.znamensky@gmail.com>
2019-06-20 17:54:53 +0300

    06, task2, done

--- 06-advanced-python/hw/task2.py
+++ 06-advanced-python/hw/task2.py
@@ -6,11 +6,23 @@ E - dict(<V> : [<V>, <V>, ...])
 Сделать так, чтобы по графу можно было итерироваться(обходом в ширину)
 
 """
-
-
 class Graph:
     def __init__(self, E):
         self.E = E
+        self.start = 0
+        self.index = len(E.keys())
+        self.key = list(E.keys())
+
+    def __next__(self):
+        if self.start >= self.index:
+            raise StopIteration()
+        x = self.start
+        self.start += 1
+        return self.key[x]
+
+
+    def __iter__(self):
+        return self
 
 
 E = {'A': ['B', 'C', 'D'], 'B': ['C'], 'C': [], 'D': ['A']}
---
+++ README.md
@@ -0,0 +1,18 @@
+Имя:        Игорь
+Фамилия:    Знаменский
+
+00-Introduction-to-Python
+    Готово
+    
+01-Data-Structures
+    json -- TODO
+    
+02-Functions
+    Готово
+    
+03-fp-decorator
+04-OOP
+05-OOP_Exceptions
+06-advanced-python
+07-networks-basics
+08-HTTP
