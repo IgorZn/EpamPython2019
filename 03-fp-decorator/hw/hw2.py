be2af9450e64761ad2a5a78151462718ed29d166
Igor Znamensky <igor.znamensky@gmail.com>
2019-06-28 17:02:21 +0300

    07, Chat, done

--- 03-fp-decorator/hw/hw2.py
+++ 03-fp-decorator/hw/hw2.py
@@ -1,7 +1,5 @@
 def is_armstrong(number):
-    length = int(len(f"{number}"))
-    digit = [x for x in map(int, f"{number}")]
-    return True if number == sum([x**length for x in digit]) else False
+    return True if number == sum([x**int(len(f"{number}")) for x in [x for x in map(int, f"{number}")]]) else False
 
 
 print(list(filter(is_armstrong, [153, 9, 10, 333, 370, 371, 407])))
--- 03-fp-decorator/hw/hw3.py
+++ 03-fp-decorator/hw/hw3.py
@@ -1,3 +1,4 @@
+import time
 count = 0
 def collatz_steps(n, count=count):
     if n == 1:
@@ -10,8 +11,19 @@ def collatz_steps(n, count=count):
         collatz_steps((n * 3) + 1, count+1)
     return a
 
+def collatz_steps_inline(n, calls=0):
+    return collatz_steps(3 * n + 1 if n % 2 else int(n / 2), calls=calls + 1) if n != 1 else calls
 
-
+start = time.time()
 assert collatz_steps(16) == 4
 assert collatz_steps(12) == 9
 assert collatz_steps(1000000) == 152
+end = time.time()
+print(end - start)
+
+start = time.time()
+assert collatz_steps_inline(16) == 4
+assert collatz_steps_inline(12) == 9
+assert collatz_steps_inline(1000000) == 152
+end = time.time()
+print(end - start)
